805948d224ac605162440ed963b4651d71fdf9d4
L1zardW1zard <45181494+L1zardW1zard@users.noreply.github.com>
2020-09-23 08:47:39 +0300

    Add files via upload

---
+++ Lab2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double nn, nk, result = 1;
+            int count=0;
+            Console.WriteLine("Enter nn: ");
+            nn = float.Parse(Console.ReadLine());
+            Console.WriteLine("Enter nk: ");
+            nk = float.Parse(Console.ReadLine());
+
+            for(int i = 1; i < nk; i++)
+            {
+
+                if (0 <= nn && nn <= nk)
+                {
+                    count++;
+                    result *= ((3 * nn) + Math.Pow(-1,(Math.Pow(nn,2) - (nn - 1))) * nn) / (2 * Math.Pow(nn,2) + 1);
+                }
+            }
+
+            Console.WriteLine("Amount of itterations: "+count+ "P="+result);
+        }
+    }
+}
