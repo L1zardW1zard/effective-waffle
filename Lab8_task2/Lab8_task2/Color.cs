4db7ee0077a7beb86a45e01f5f9a6e84e27afc76
L1zardW1zard <45181494+L1zardW1zard@users.noreply.github.com>
2020-12-11 20:20:58 +0200

    Лаб 8 задание 2

---
+++ Lab8_task2/Lab8_task2/Color.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab8_task2
+{
+    class ColorScreenPhone : BlackWhitePhone
+    {
+        public bool wifi = true;
+        public override void Call()
+        {
+            Console.WriteLine("Call anyone you click on.");
+        }
+        public override void RingTone()
+        {
+            Console.WriteLine("*plays a song downloaded from the Internet*");
+        }
+        public virtual void SurfTheNet()
+        {
+            Console.WriteLine("KEKW");
+        }
+
+    }
+}
---
+++ Lab8_task2/Lab8_task2/WB.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab8_task2
+{
+    class WBPhone : ButtonPhone
+    {
+        public bool display = true;
+        public override void Call()
+        {
+            Console.WriteLine("Hi-hi! Personally I can call to someone using display!");
+        }
+
+        public override void RingTone()
+        {
+            Console.WriteLine("*plays Nokia 3310 ringtone*");
+        }
+    }
+}
